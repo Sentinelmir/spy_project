5f81d3319d4bd8f9ca1d0e6ca8aed9ff9ad82793
Arina M <sentinelmir@gmail.com>
2025-08-13 12:43:59 +0300

    Add js, chose random word function, buttons to change page

---
+++ skript.js
@@ -0,0 +1,69 @@
+const startGameBtn = document.querySelector('#startBtn');
+const numberOfPlayers = document.querySelector('#players');
+const numberOfSpies = document.querySelector('#spies');
+const themeSelector = document.querySelector('#category');
+const newGameBtn = document.querySelector('#start_new_game');
+const showWordBtn = document.querySelector('#show_word_button');
+const hideWordBtn = document.querySelector('#hide_word_button');
+let word;
+
+const words = {
+  places: [
+        "Библиотека", "Школа", "Аэропорт", "Пляж", "Музей", "Кафе", "Рынок", "Театр", "Стадион", "Парк",
+        "Метро", "Автовокзал", "Гостиница", "Кладбище", "Ферма", "Супермаркет", "Ресторан", "Офис", "Банк", "Зоопарк",
+        "Бассейн", "Кинотеатр", "Планетарий", "Тюрьма", "Вокзал", "Аптека", "Церковь", "Детский сад", "Салон красоты", "Магазин игрушек",
+        "Галерея", "Танцевальная студия", "Площадь", "Обсерватория", "Лес", "Бар", "Концертный зал", "Парк аттракционов", "Гараж", "Суд",
+        "Винодельня", "Спортзал", "Пекарня", "Больница", "Порт", "Фабрика", "Гора", "Озеро", "Дворец", "Пещера"
+    ],
+    hobbies: [
+        "Рисование", "Фотография", "Чтение", "Пение", "Игра на гитаре", "Плавание", "Йога", "Бег", "Танцы", "Шахматы",
+        "Вышивка", "Рыбалка", "Велоспорт", "Кулинария", "Катание на коньках", "Скейтбординг", "Садоводство", "Путешествия", "Кемпинг", "Пение в караоке",
+        "Сноуборд", "Катание на лыжах", "Пазлы", "Настольные игры", "Ролики", "Кроссворды", "Фехтование", "Каллиграфия", "Флористика", "Пение в хоре",
+        "Керамика", "Фигурное катание", "Боевые искусства", "Моделизм", "Картинг", "Астрономия", "Археология", "Серфинг", "Пение в душе", "Блогинг",
+        "Пение на сцене", "Видеомонтаж", "Гейминг", "Фридайвинг", "Вышивание бисером", "Реставрация мебели", "Пение в парке", "Коллекционирование марок", "Оригами", "Караоке"
+    ],
+    food: [
+        "Пицца", "Суши", "Бургер", "Паста", "Салат", "Шашлык", "Борщ", "Сырники", "Омлет", "Блины",
+        "Пельмени", "Вареники", "Плов", "Котлета", "Картошка фри", "Сэндвич", "Торт", "Мороженое", "Пирог", "Смузи",
+        "Стейк", "Курица", "Рыба", "Овощи", "Фрукты", "Суп", "Шоколад", "Мюсли", "Йогурт", "Гречка",
+        "Рис", "Печенье", "Чизкейк", "Мороженое с шоколадом", "Салат Цезарь", "Паста Карбонара", "Пицца Маргарита", "Крабовые палочки", "Пельмени с мясом", "Каша овсяная",
+        "Сэндвич с ветчиной", "Салат Оливье", "Бифштекс", "Куриные крылышки", "Лазанья", "Кебаб", "Гуляш", "Панини", "Фахитос", "Рамен"
+    ]
+};
+
+function getRandomWord(category) {
+  const categoryWords = words[category];
+  const randomIndex = Math.floor(Math.random() * categoryWords.length);
+  return categoryWords[randomIndex];
+}
+
+startGameBtn.addEventListener('click', startNewGame);
+newGameBtn.addEventListener('click', endTheGame);
+
+
+function startNewGame(e){
+    e.preventDefault();
+
+    const playersNumber = numberOfPlayers.value.trim();
+    const spiesNumber = numberOfSpies.value.trim();
+    const theme = themeSelector.value.trim();
+
+    if (!playersNumber || !spiesNumber || !theme) {
+        return;
+      }
+
+    document.getElementById('first_section_id').classList.add('hidden');
+    document.getElementById('second_section_id').classList.remove('hidden');
+    hideWordBtn.disabled = true;
+    word = getRandomWord(theme);
+
+    console.log(word);
+
+}
+
+function endTheGame(e){
+    e.preventDefault();
+
+    document.getElementById('second_section_id').classList.add('hidden');
+    document.getElementById('first_section_id').classList.remove('hidden');
+}
